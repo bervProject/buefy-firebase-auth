664d4ddb2ff9701f0899f61ea32aba330ca11459
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-01-14 11:41:09 +0700

    Update Style and Add Update Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
-  <section>
+  <section class="section">
     <div class="container">
       <div class="content has-text-centered">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld :msg="`Hello ${this.email}`" />
-        <p class="content">Email: {{ email }}</p>
-        <button class="button is-danger" @click="logout">Logout</button>
+        <h1 class="title">
+          Firebase Auth Demo
+        </h1>
+        <section class="section">
+          <div class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img v-if="photoUrl" :src="photoUrl" />
+                    <img
+                      v-else
+                      src="https://bulma.io/images/placeholders/96x96.png"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p v-if="name" class="title is-4">{{ name }}</p>
+                  <p v-else class="title is-4">User</p>
+                  <p class="subtitle is-6">{{ email }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <h2 class="title">Update Profile</h2>
+          <b-field label="Name">
+            <b-input type="text" v-model="input.name"></b-input>
+          </b-field>
+          <b-field label="Photo URL">
+            <b-input type="url" v-model="input.photoURL"></b-input>
+          </b-field>
+          <div class="buttons">
+            <button class="button is-link" @click="update">Update</button>
+            <button class="button is-danger" @click="logout">Logout</button>
+          </div>
+        </section>
       </div>
     </div>
   </section>
@@ -14,17 +49,19 @@
 <script>
 // @ is an alias to /src
 import firebase from "firebase/app";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   data() {
     return {
-      email: ""
+      name: "",
+      photoUrl: "",
+      email: "",
+      input: {
+        name: "",
+        photoURL: ""
+      }
     };
-  },
-  components: {
-    HelloWorld
   },
   methods: {
     logout: function() {
@@ -34,12 +71,44 @@ export default {
         .then(() => {
           this.$router.replace("login");
         });
+    },
+    update: function() {
+      const user = firebase.auth().currentUser;
+      if (user != null) {
+        const loadingComponent = this.$buefy.loading.open({
+          container: true
+        });
+        user
+          .updateProfile({
+            displayName: this.input.name,
+            photoURL: this.input.photoURL
+          })
+          .then(() => {
+            loadingComponent.close();
+            this.updateProfile(firebase.auth().currentUser);
+          })
+          .catch(error => {
+            loadingComponent.close();
+            this.$buefy.toast.open({
+              message: `Error: ${error.message}`,
+              type: "is-danger",
+              duration: 5000
+            });
+          });
+      }
+    },
+    updateProfile: function(currentUser) {
+      this.email = currentUser.email;
+      this.name = currentUser.displayName;
+      this.photoUrl = currentUser.photoURL;
     }
   },
   mounted: function() {
     const currentUser = firebase.auth().currentUser;
     if (currentUser) {
-      this.email = currentUser.email;
+      this.updateProfile(currentUser);
+      this.input.name = currentUser.displayName;
+      this.input.photoURL = currentUser.photoURL;
     }
   }
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,46 @@
 <template>
   <section>
     <div class="container">
-      <h1 id="title" class="title has-text-centered">
-        Vue Bulma with Firebase Auth
-      </h1>
-      <div id="login-box" class="box">
-        <h1 class="title">Login</h1>
-        <ValidationObserver v-slot="{ handleSubmit }">
-          <form @submit.prevent="handleSubmit(login)">
-            <ValidationProvider
-              name="email"
-              rules="required|email"
-              v-slot="{ errors, failed, passed }"
-            >
-              <b-field
-                label="Email"
-                :type="{ 'is-danger': failed, 'is-success': passed }"
-                :message="errors[0]"
+      <h1 id="title" class="title has-text-centered">Vue Bulma with Firebase Auth</h1>
+      <div id="login-box" class="card">
+        <div class="card-content">
+          <h1 class="title">Login</h1>
+          <ValidationObserver v-slot="{ handleSubmit }">
+            <form @submit.prevent="handleSubmit(login)">
+              <ValidationProvider
+                name="email"
+                rules="required|email"
+                v-slot="{ errors, failed, passed }"
               >
-                <b-input type="email" v-model="email" maxlength="30"></b-input>
-              </b-field>
-            </ValidationProvider>
-            <ValidationProvider
-              name="password"
-              rules="required"
-              v-slot="{ errors, failed, passed }"
-            >
-              <b-field
-                label="Password"
-                :type="{ 'is-danger': failed, 'is-success': passed }"
-                :message="errors[0]"
+                <b-field
+                  label="Email"
+                  :type="{ 'is-danger': failed, 'is-success': passed }"
+                  :message="errors[0]"
+                >
+                  <b-input type="email" v-model="email" maxlength="30"></b-input>
+                </b-field>
+              </ValidationProvider>
+              <ValidationProvider
+                name="password"
+                rules="required"
+                v-slot="{ errors, failed, passed }"
               >
-                <b-input
-                  type="password"
-                  v-model="password"
-                  password-reveal
-                ></b-input>
-              </b-field>
-            </ValidationProvider>
-
-            <button class="button is-primary">Login</button>
-          </form>
-        </ValidationObserver>
+                <b-field
+                  label="Password"
+                  :type="{ 'is-danger': failed, 'is-success': passed }"
+                  :message="errors[0]"
+                >
+                  <b-input type="password" v-model="password" password-reveal></b-input>
+                </b-field>
+              </ValidationProvider>
+              <section class="section">
+                <div class="content has-text-centered">
+                  <button class="button is-primary">Login</button>
+                </div>
+              </section>
+            </form>
+          </ValidationObserver>
+        </div>
       </div>
     </div>
   </section>
@@ -60,19 +59,24 @@ export default {
   },
   methods: {
     login: function() {
+      const loadingComponent = this.$buefy.loading.open({
+        container: true
+      });
       firebase
         .auth()
         .signInWithEmailAndPassword(this.email, this.password)
         .then(
           user => {
+            loadingComponent.close();
             this.$router.replace("home");
             this.$buefy.toast.open({
-              message: `Welcome home ${user.user.displayName}`,
+              message: `Welcome home ${user.user.email}`,
               type: "is-success",
               duration: 5000
             });
           },
           err => {
+            loadingComponent.close();
             this.$buefy.toast.open({
               message: `Error: ${err.message}`,
               type: "is-danger",
